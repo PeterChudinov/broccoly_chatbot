4603cb50b6d921718435c9e6053c3323c1b9f883
Rustam Zagirov <rustam.zagirov@lazada.com>
2016-06-25 20:11:27 +0300

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625170714) do
+ActiveRecord::Schema.define(version: 20160625171045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20160625170714) do
     t.string   "facebook_id"
     t.string   "first_name"
     t.string   "last_name"
-    t.integer  "gender"
+    t.string   "gender"
     t.string   "type"
     t.string   "style"
     t.string   "price"
