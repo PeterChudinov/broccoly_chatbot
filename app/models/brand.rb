71551be3b01ee136f183d44e07791a23729cb73f
Peter Chudinov <peterchudinov@icloud.com>
2016-06-25 19:38:30 +0300

    import brands

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,8 @@
 class Brand < ActiveRecord::Base
-	has_many :matches
+  has_many :matches
+
+  def self.import("/populate.csv")
+    CSV.foreach(file.path, headers: true) do |row|
+      Brand.create! row.to_hash
+    end
 end
