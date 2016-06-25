11f8c0e4621ee7ee73bdb4c74e37691dc836d7a4
Rustam Zagirov <rustam.zagirov@lazada.com>
2016-06-25 17:46:10 +0300

    test message

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,6 +1,13 @@
 class MessengerController < Messenger::MessengerController
   def webhook
-    #logic here
+    if fb_params.first_entry.callback.message?
+      Messenger::Client.send(
+          Messenger::Request.new(
+              Messenger::Elements::Text.new(text: 'some text'),
+              fb_params.first_entry.sender_id
+          )
+      )
+    end
     render nothing: true, status: 200
   end
 end
