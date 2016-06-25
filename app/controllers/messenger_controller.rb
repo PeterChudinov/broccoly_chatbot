9d6d3eaed356334c4465a5739c0ac64b4e0df44b
Rustam Zagirov <rustam.zagirov@lazada.com>
2016-06-25 19:46:50 +0300

    add saving to db

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -4,6 +4,16 @@ class MessengerController < Messenger::MessengerController
     logger.debug "message?: #{fb_params.message?}
 delivery?: #{fb_params.delivery?}
 postback?: #{fb_params.postback?}"
+
+    unless User.exists?(facebook_id: fb_params.sender_id)
+      fb_data = JSON.parse(Messenger::Client.get_user_profile(fb_params.sender_id))
+      User.create(
+        facebook_id: fb_params.sender_id,
+        first_name: fb_data['first_name'],
+        last_name: fb_data['last_name']
+      )
+
+    end
     if fb_params.message?
       Messenger::Client.send(
           Messenger::Request.new(
@@ -12,7 +22,9 @@ postback?: #{fb_params.postback?}"
           )
       )
     elsif fb_params.postback?
-      if fb_params.send(:messaging_entry)['postback']['payload'] == 'lets starts'
+      value = fb_params.send(:messaging_entry)['postback']['payload']
+      case value
+      when 'lets starts'
         buttons = Messenger::Templates::Buttons.new(
           text: 'Select gender you identity with',
           buttons: [
@@ -36,6 +48,8 @@ postback?: #{fb_params.postback?}"
         Messenger::Client.send(
             Messenger::Request.new(buttons, fb_params.sender_id)
         )
+        when 'male', 'female', 'neutral'
+          User.where(facebook_id: fb_params.sender_id).update_all(gender: value)
       end
     end
     render nothing: true, status: 200
