50f1075cb68cabbf65e2b874281c40debd81df04
Rustam Zagirov <rustam.zagirov@lazada.com>
2016-06-25 21:10:41 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,10 @@ class User < ActiveRecord::Base
 
 
 	def matching_brands
-		user = User.where(facebook_id: self.facebook_id)
 		types = %i{gender, type, style, price, music, mood, personality}
 		where = {}
 		types.each do |type|
-			where[type] = user.attributes[type.to_s]
+			where[type] = self.attributes[type.to_s]
 		end
 		ids = BrandOption.where(where).group(:brand_id).order('COUNT(*) DESC').pluck(:brand_id)
 		Brand.where(id: ids)
