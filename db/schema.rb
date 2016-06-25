4a52fa63cd4cc1bc66443e83a46217166aea1582
Rustam Zagirov <rustam.zagirov@lazada.com>
2016-06-25 19:55:16 +0300

    change type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625152754) do
+ActiveRecord::Schema.define(version: 20160625165359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20160625152754) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "facebook_id"
+    t.string   "facebook_id"
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "gender"
