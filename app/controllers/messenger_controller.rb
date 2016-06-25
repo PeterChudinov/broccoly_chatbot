eef7852fc05ff17fe7405454563b43a8b1b512e2
Rustam Zagirov <rustam.zagirov@lazada.com>
2016-06-25 19:30:40 +0300

    second step

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -5,13 +5,38 @@ class MessengerController < Messenger::MessengerController
 delivery?: #{fb_params.delivery?}
 postback?: #{fb_params.postback?}"
     if fb_params.message?
-
       Messenger::Client.send(
           Messenger::Request.new(
               Messenger::Elements::Text.new(text: "Your wrote #{fb_params.text_message}"),
               fb_params.sender_id
           )
       )
+    elsif fb_params.postback?
+      if fb_params.send(:messaging_entry)['postback']['payload'] == 'lets starts'
+        buttons = Messenger::Templates::Buttons.new(
+          text: 'Select gender you identity with',
+          buttons: [
+            Messenger::Elements::Button.new(
+              type: 'postback',
+              title: 'Female',
+              payload: 'female'
+            ),
+            Messenger::Elements::Button.new(
+              type: 'postback',
+              title: 'Male',
+              payload: 'male'
+            ),
+            Messenger::Elements::Button.new(
+              type: 'postback',
+              title: 'Neutral',
+              payload: 'neutral'
+            ),
+          ]
+        )
+        Messenger::Client.send(
+            Messenger::Request.new(buttons, fb_params.sender_id)
+        )
+      end
     end
     render nothing: true, status: 200
   end
