e74dceeddb4a9ed12e1e0fe3ced2d40b5baadd19
Peter Chudinov <peterchudinov@icloud.com>
2016-06-25 17:38:30 +0300

    brand_option

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625121809) do
+ActiveRecord::Schema.define(version: 20160625143700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brand_options", force: :cascade do |t|
+    t.integer  "brand_id"
+    t.string   "key"
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "brands", force: :cascade do |t|
     t.datetime "created_at",  null: false
