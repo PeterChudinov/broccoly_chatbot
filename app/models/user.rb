f56691cf47b6c4a394b66542cf90021f1af4c879
Rustam Zagirov <rustam.zagirov@lazada.com>
2016-06-25 21:19:24 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,16 @@ class User < ActiveRecord::Base
 
 	def matching_brands
 		types = %i{gender type style price music mood personality}
-		where = {}
+		where = []
+		args = []
 		types.each do |type|
 			value = self.attributes[type.to_s]
 			unless value.nil?
-				where[type] = value.upcase
+				where.push('(key = ? AND value=?)')
+				args.push(type, value.upcase)
 			end
 		end
-		ids = BrandOption.where(where).group(:brand_id).order('COUNT(*) DESC').pluck(:brand_id)
+		ids = BrandOption.where(where.join(' AND '), *args).group(:brand_id).order('COUNT(*) DESC').pluck(:brand_id)
 		Brand.where(id: ids)
 	end
 end
